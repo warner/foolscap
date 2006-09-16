ce6751c248c1774bd629f28f62d416638e5585ac
Brian Warner <warner@lothar.com>
2006-09-16 20:47:52 +0000

    [foolscap @ initial import of foolscap from SVN]

---
+++ foolscap/__init__.py
@@ -0,0 +1,13 @@
+"""Foolscap"""
+
+__version__ = "0.0.2+"
+
+# here are the primary entry points
+from foolscap.pb import PBService, getRemoteURL_TCP
+
+# names we import so that others can reach them as foolscap.foo
+from foolscap.remoteinterface import RemoteInterface
+from foolscap.referenceable import Referenceable, SturdyRef
+from foolscap.copyable import Copyable, RemoteCopy, registerRemoteCopy
+from foolscap.ipb import DeadReferenceError
+from foolscap.tokens import BananaError
