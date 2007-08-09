959353910f8586ca0c2063231d2b82779426e2e9
Brian Warner <warner@lothar.com>
2007-08-09 00:43:52 -0700

    setup.py: add classifiers= and other metadata. Closes #7.

--- setup.py
+++ setup.py
@@ -10,8 +10,9 @@ if __name__ == '__main__':
         version=__version__,
         description="Foolscap contains an RPC protocol for Twisted.",
         author="Brian Warner",
-        author_email="warner@twistedmatrix.com",
-        url="http://twistedmatrix.com/trac/wiki/FoolsCap",
+        author_email="warner-foolscap@lothar.com",
+        url="http://foolscap.lothar.com/",
+        download_url="http://foolscap.lothar.com/releases/foolscap-0.1.5.tar.gz",
         license="MIT",
         long_description="""\
 Foolscap (aka newpb) is a new version of Twisted's native RPC protocol, known
@@ -19,5 +20,18 @@ as 'Perspective Broker'. This allows an object in one process to be used by
 code in a distant process. This module provides data marshaling, a remote
 object reference system, and a capability-based security model.
 """,
+        classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: System :: Distributed Computing",
+        "Topic :: System :: Networking",
+        "Topic :: Software Development :: Object Brokering",
+        ],
+        platforms=["any"],
+        
         packages=["foolscap", "foolscap/slicers", "foolscap/test"],
         )
