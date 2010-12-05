89ee821572a223f43a88bb1abdc4f155c60f7f7d
Brian Warner <warner@lothar.com>
2010-12-05 13:41:18 -0600

    empty foolscap/__init__.py, finishing the #122 transition to foolscap.api

--- foolscap/__init__.py
+++ foolscap/__init__.py
@@ -2,37 +2,5 @@
 
 from _version import verstr as __version__
 
-from foolscap import schema as _for_side_effect # register adapters
-
-# All names here are now deprecated. Please import them from foolscap.api
-# instead. This file will become empty (except for __version__) in the next
-# major release.
-
-# here are the primary entry points
-from deprecated import Tub, UnauthenticatedTub
-from deprecated import RemoteInterface, Referenceable, SturdyRef
-
-# names we import so that others can reach them as foolscap.foo
-from deprecated import Copyable, RemoteCopy, registerRemoteCopy
-from deprecated import registerCopier, registerRemoteCopyFactory
-from deprecated import DeadReferenceError
-from deprecated import BananaError
-from deprecated import schema
-from deprecated import serialize, unserialize
-
 # hush pyflakes
-_unused = [
-    __version__,
-    _for_side_effect,
-    Tub, UnauthenticatedTub,
-    RemoteInterface,
-    Referenceable, SturdyRef,
-    Copyable, RemoteCopy, registerRemoteCopy,
-    registerCopier, registerRemoteCopyFactory,
-    DeadReferenceError,
-    BananaError,
-    schema,
-    serialize, unserialize,
-    ]
-del _unused
-del _for_side_effect
+_unused = __version__; del _unused
