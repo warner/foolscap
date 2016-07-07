b4e8e9e248e95b8a239b51a131416210c53bd090
Brian Warner <warner@lothar.com>
2016-07-06 21:23:11 -0700

    more

--- test-ports.py
+++ test-ports.py
@@ -6,16 +6,36 @@ print "platformType:", platformType
 target = int(sys.argv[1])
 print "allocating ports, will stop if we're given %d" % target
 
-def allocate_tcp_port():
-    """Return an (integer) available TCP port on localhost. This briefly
-    listens on the port in question, then closes it right away."""
+listen_failed = set()
+
+def _make_socket():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     if platformType == "posix" and sys.platform != "cygwin":
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    #s.bind(("127.0.0.1", 0))
-    s.bind(("0.0.0.0", 0))
-    port = s.getsockname()[1]
-    s.close()
+    return s
+
+def allocate_tcp_port():
+    """Return an (integer) available TCP port on localhost. This briefly
+    listens on the port in question, then closes it right away."""
+    while True:
+        s = _make_socket()
+        #s.bind(("127.0.0.1", 0))
+        s.bind(("0.0.0.0", 0))
+        port = s.getsockname()[1]
+        s.close()
+        if port in tried:
+            return None
+        tried.add(port)
+        s = _make_socket()
+        try:
+            s.bind(("0.0.0.0", port))
+            s.listen(5)
+            s.close()
+            return port
+        except socket.error:
+            listen_failed.add(port)
+            s.close()
+            # try again
     return port
 
 def test(port, do_listen):
@@ -34,11 +54,10 @@ for i in itertools.count():
         print "reached iteration limit, exiting"
         break
     port = allocate_tcp_port()
-    if port in tried:
-        print "looped around, exiting"
+    if port is None:
+        print "got same port twice, exiting", len(tried)
         break
-    tried.add(port)
-    #print port
+    print port
     try:
         test(port, not True)
     except socket.error as e:
@@ -50,6 +69,9 @@ for i in itertools.count():
         break
 
 print "tried", min(tried), "-", max(tried)
+refrained = set(range(min(tried), max(tried)+1)) - tried
+print "refrained:", sorted(refrained)
+print "listen() precheck failed", sorted(listen_failed)
 print "failures:", sorted(failed)
 
 # on OS-X, s.bind(("0.0.0.0"),0).getsockname()[1];s.close() will allocate you
