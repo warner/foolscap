00ac58d72b3b38ee8b25409bac6737d6778d214e
Brian Warner <warner@lothar.com>
2019-03-28 10:00:46 -0700

    remove 'i2p' extra and dependency on py2-only 'txi2p'

--- setup.py
+++ setup.py
@@ -61,9 +61,8 @@ object reference system, and a capability-based security model.
     "cmdclass": commands,
     "install_requires": ["twisted[tls] >= 16.0.0", "pyOpenSSL"],
     "extras_require": {
-        "dev": ["mock", "txtorcon >= 0.16.1", "txi2p >= 0.3.2"],
+        "dev": ["mock", "txtorcon >= 0.16.1"],
         "tor": ["txtorcon >= 0.16.1"],
-        "i2p": ["txi2p >= 0.3.2"],
         },
 }
 
