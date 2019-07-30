c5d4814ee1d69d2ded7ddc6c6b2440209cec3c33
Anand B Pillai <anandpillai@letterboxes.org>
2019-07-30 15:26:02 +0530

    Making test_banana pass on Python3

--- src/foolscap/slicers/dict.py
+++ src/foolscap/slicers/dict.py
@@ -1,5 +1,6 @@
 # -*- test-case-name: foolscap.test.test_banana -*-
 
+import six
 from twisted.python import log
 from twisted.internet.defer import Deferred
 from foolscap.tokens import Violation, BananaError
@@ -12,7 +13,7 @@ class DictSlicer(BaseSlicer):
     trackReferences = True
     slices = None
     def sliceBody(self, streamable, banana):
-        for key,value in self.obj.items():
+        for key,value in list(self.obj.items()):
             yield key
             yield value
 
@@ -91,7 +92,7 @@ class DictUnslicer(BaseUnslicer):
         if isinstance(key, Deferred):
             raise BananaError("incomplete object as dictionary key")
         try:
-            if self.d.has_key(key):
+            if key in self.d:
                 raise BananaError("duplicate key '%s'" % key)
         except TypeError:
             raise BananaError("unhashable key '%s'" % key)
@@ -119,7 +120,7 @@ class DictUnslicer(BaseUnslicer):
 class OrderedDictSlicer(DictSlicer):
     slices = dict
     def sliceBody(self, streamable, banana):
-        keys = self.obj.keys()
+        keys = list(self.obj.keys())
         keys.sort()
         for key in keys:
             value = self.obj[key]
@@ -128,7 +129,7 @@ class OrderedDictSlicer(DictSlicer):
 
 
 class DictConstraint(OpenerConstraint):
-    opentypes = [("dict",)]
+    opentypes = [(six.b("dict"),)]
     name = "DictConstraint"
 
     def __init__(self, keyConstraint, valueConstraint, maxKeys=None):
@@ -137,11 +138,11 @@ class DictConstraint(OpenerConstraint):
         self.maxKeys = maxKeys
     def checkObject(self, obj, inbound):
         if not isinstance(obj, dict):
-            raise Violation, "'%s' (%s) is not a Dictionary" % (obj,
-                                                                type(obj))
+            raise Violation("'%s' (%s) is not a Dictionary" % (obj,
+                                                                type(obj)))
         if self.maxKeys != None and len(obj) > self.maxKeys:
-            raise Violation, "Dict keys=%d > maxKeys=%d" % (len(obj),
-                                                            self.maxKeys)
-        for key, value in obj.iteritems():
+            raise Violation("Dict keys=%d > maxKeys=%d" % (len(obj),
+                                                            self.maxKeys))
+        for key, value in obj.items():
             self.keyConstraint.checkObject(key, inbound)
             self.valueConstraint.checkObject(value, inbound)
