ca205d8e7f6c149b54285a6da0466f2d5d95f302
Brian Warner <warner@lothar.com>
2019-12-22 22:41:44 -0500

    base32: notes

--- src/foolscap/base32.py
+++ src/foolscap/base32.py
@@ -1,6 +1,6 @@
 import sys, base64
 
-def encode(b):
+def encode(b): # takes bytes, returns native string
     assert isinstance(b, bytes), (type(b), b)
     out = base64.b32encode(b).lower().rstrip(b"=")
     if sys.version_info.major == 2:
