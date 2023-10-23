ef28e527b9e153859cedaca92a10a53e67002d1c
Itamar Turner-Trauring <itamar@pythonspeed.com>
2023-10-23 08:44:06 -0400

    Upgrade versioneer

--- src/foolscap/__init__.py
+++ src/foolscap/__init__.py
@@ -1,5 +1,6 @@
 """Foolscap"""
 
-from ._version import get_versions
-__version__ = str(get_versions()['version'])
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
+
+del _version
